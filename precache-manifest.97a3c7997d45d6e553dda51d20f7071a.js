a77c813d63329adf58e5ecc7a53d7e92251dca88
Master-Maso <masoud_ms2021@yahoo.com>
2019-07-08 12:27:49 +0100

    Updates

---
+++ precache-manifest.97a3c7997d45d6e553dda51d20f7071a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d43fd9d94abeb0ec2a76ccbe769fc999",
+    "url": "/maktabkhooneh-resume/index.html"
+  },
+  {
+    "revision": "f4a258cd47d5b955ee50",
+    "url": "/maktabkhooneh-resume/static/css/main.0b32b57d.chunk.css"
+  },
+  {
+    "revision": "1550d7175d714246553c",
+    "url": "/maktabkhooneh-resume/static/js/2.0689f26d.chunk.js"
+  },
+  {
+    "revision": "f4a258cd47d5b955ee50",
+    "url": "/maktabkhooneh-resume/static/js/main.2fd811fc.chunk.js"
+  },
+  {
+    "revision": "45b9c8c53c69b1e9af3b",
+    "url": "/maktabkhooneh-resume/static/js/runtime~main.a3d41e9b.js"
+  }
+]);
